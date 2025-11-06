b42f82c35af89fb689cec92abcb4801cec1e55c8
umiswing <umiswing@foxmail.com>
2025-11-06 18:21:35 +0800

    Revert "[Distributed] Add PipelineDatasetPreprocessor to aviod mem leaks in pipeline parallel (#75472)"
    This reverts commit c6d3dc506c6bd6556e778e1b7a783807fd349ff9.

--- python/paddle/distributed/fleet/meta_parallel/__init__.py
+++ python/paddle/distributed/fleet/meta_parallel/__init__.py
@@ -31,7 +31,6 @@ from .pipeline_parallel import (  # noqa: F401
     PipelineParallelMicroStepLocations,
     PipelineParallelWithInterleave,
     PipelineParallelWithInterleaveFthenB,
-    PipelineDatasetPreprocessor,
     VPPFhenBInBalancedMemory,
     register_global_pipeline_parallel_hook,
 )
